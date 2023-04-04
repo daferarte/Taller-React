9c5fd20a093620ada16deda34ae8a02221f2da14
daferarte <daferarte@gmail.com>
2023-04-04 18:13:21 -0500

    Definiendo componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import './style.css';
 
-const nombres = [
-  'daniel',
-  'Eduardo',
-  'Marines',
-  'Cody'
-]
 
-const Nombres = () => {
-  return <ul>{
-    nombres.map( (nombre, index) => <li key={index}>{nombre}</li>) //iteracion de arreglos con map
-  }</ul> 
-}
 
 export default function App() {
   return (
     <div>
-      <Nombres />
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React, { StrictMode } from 'react';
+import React, { StrictMode, Component } from 'react'; //cada componente que se usa en react debe importar la libreria react
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+//import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+// const App = () => <h1>hola mundo</h1> // componente basico resumido
+
+class App extends Component{ //componente en clase
+  
+  render(){ //metodo render que retorna el componente
+    return <h1>Hola mundo</h1>
+  }
+}
 
 root.render(
   <StrictMode>
