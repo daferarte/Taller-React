740be210cb110404913c22e1e6238bba31df4751
daferarte <daferarte@gmail.com>
2023-04-04 16:43:11 -0500

    condicionales basico

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './style.css';
 
+function SaludarEnIdiomas({idioma}){
+  if(idioma === "es") {
+    return <span>Hola</span>;
+  }else{
+    return <span>Hello</span>;
+  }
+}
+
 const Saludo = () => {
-  return <p>hola</p>;
+  const nombre = "Daniel";
+  return <p><SaludarEnIdiomas idioma={"en"}/> { `${nombre}` } </p>;
 };
 
 export default function App() {
