69002cf67562c119690bdfad9d7522a6204559de
daferarte <daferarte@gmail.com>
2023-04-04 18:28:05 -0500

    uso de props basico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './style.css';
 
+//metodo sin desestructuracion
+// const Saludar = (props) => {
+//   return <p>Hola {props.nombre} </p>
+// }
 
+//metodo con desestructuracion
+// const Saludar = ({nombre}) => {
+//   return <p>Hola {nombre} </p>
+// }
+
+//multiples variables
+const Saludar = ({nombre, idioma}) => {
+  const saludo = idioma === "es" ? "'Hola":"Hello";
+  return <p>{saludo} {nombre} </p>
+}
 
 export default function App() {
   return (
     <div>
-
+      {/* una variable */}
+      {/* <Saludar nombre='Daniel' /> */}
+      {/* multiples variales */}
+      <Saludar nombre='Daniel' idioma='es' />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React, { StrictMode, Component } from 'react'; //cada componente que se usa en react debe importar la libreria react
 import { createRoot } from 'react-dom/client';
 
-//import App from './App';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-// const App = () => <h1>hola mundo</h1> // componente basico resumido
-
-class App extends Component{ //componente en clase
-  
-  /**
-   * metodo render que retorna el componente, cada vez que guarda se ejecuta o encuentra un cambio
-   * React hace lo menos posible para actualizar la interfaz
-   */
-  render(){ 
-    return <h1>Hola mundo</h1>
-  }
-}
 
 root.render(
   <StrictMode>
