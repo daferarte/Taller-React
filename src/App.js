bd35f1d4234854def8ea35dc57e627ee1d94ad6e
daferarte <daferarte@gmail.com>
2023-04-04 20:15:17 -0500

    useEffect solo ejecuta al iniciar el componente

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const Button = () => {
    */
   useEffect(()=>{
     console.log("me ejecuto"); // se ejecuta con cada actualizacion
-  })
+  },[]); //solo se ejecuta al iniciar el componente
   return(
     <div>
       <button onClick={()=>setCount(count+1)}>Enviar!</button> 
