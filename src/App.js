732f2eb15d854041328a3379208e978adb7261cc
daferarte <daferarte@gmail.com>
2023-04-05 10:19:35 -0500

    envio de peticion jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -3,24 +3,51 @@ import './style.css';
 
 //formularios
 
-//lectura de datos
+//Enviando formularios
+
 // usar JSONPlaceholder
+
+
 
 //Formulario
 const Form = () =>{
   //useState se usa para optener el valor de las variables
   let [title,setTitle] = useState("");
   let [body,setBody] = useState("");
+
+  const sendForm = (ev)=>{
+    ev.preventDefault() // previene el cargue tradicional del formulario
+    /**
+     * fake api de JSONPlaceholder para similar envio por ajax
+     */
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json()) //respuesta
+      .then((json) => console.log(json));
+  }
+
   return (
-    <form>
+    // evento onsubmit para enviar el formulario se envia el evento del formulario
+    <form onSubmit={(ev)=> sendForm(ev)}> 
       <div>
-        <label for="title">Titulo</label>
+        <label htmlFor="title">Titulo</label>
         <input type='text' id='title' onChange={ (evt)=> setTitle(evt.target.value)}></input>
       </div>
       <div>
-        <label for="body">Publicacion</label>
+        <label htmlFor="body">Publicacion</label>
         <textarea id='body' onChange={ (evt)=> setBody(evt.target.value)}></textarea>
       </div>
+      {/* no romper html creando funciones innecesarios html ya las hace */}
+      {/* <input onClick={()=>{ /*enviar formulario }} />  */}
       <input type='submit' value="enviar" />
     </form>
   )
