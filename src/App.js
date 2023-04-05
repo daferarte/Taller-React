75e4510630521e20f839922d0492aba4ff5ec6e7
daferarte <daferarte@gmail.com>
2023-04-05 08:59:51 -0500

    Formularios, Capruta de datos useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './style.css';
 
-//eventos
+//formularios
 
-const Saludo = () =>{
-  const [name, setName]=useState("")
-  return(
-    <div>
-      {/* syntheticEvent */}
-      {/* <input type='text' onChange={(ev)=>console.log(ev)}></input> */}
-      {/* enparejamiento de una funcion cuando se ejecuta con un evento */}
-      <input type='text' onChange={(ev)=>setName(ev.target.value)}></input> 
-      <p>hola {name}</p>
-    </div>
+//lectura de datos
+// usar JSONPlaceholder
+
+//Formulario
+const Form = () =>{
+  //useState se usa para optener el valor de las variables
+  let [title,setTitle] = useState("");
+  let [body,setBody] = useState("");
+  return (
+    <form>
+      <div>
+        <label for="title">Titulo</label>
+        <input type='text' id='title' onChange={ (evt)=> setTitle(evt.target.value)}></input>
+      </div>
+      <div>
+        <label for="body">Publicacion</label>
+        <textarea id='body' onChange={ (evt)=> setBody(evt.target.value)}></textarea>
+      </div>
+      <input type='submit' value="enviar" />
+    </form>
   )
 }
+
+
 
 
 export default function App() {
   return (
     <div>
-      <Saludo />
+      <Form />
     </div>
   );
 }
