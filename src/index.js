19f01e0de20ac953ea2c9caed3b4f0616ac7cc92
daferarte <daferarte@gmail.com>
2023-04-06 11:18:17 -0500

    tipos de datos en props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
 class App extends React.Component{
   static defaultProps ={
     name: "pepito" //props por defecto en caso de que no se asignen
+  }
+
+  static propTypes ={ //sirve para definir el tipo de datos que puede tener un prop
+    name: PropTypes.string
   }
   render(){
     console.log(this.props); //props de solo lectura no se pueden reaccionar
@@ -11,4 +16,5 @@ class App extends React.Component{
   }
 }
 
-render(<App />, document.getElementById('root')); //definicion de props
+// render(<App name={2}/>, document.getElementById('root')); //prop incorrecta genera warning pero la ejecucion sigue
+render(<App name="daniel" />, document.getElementById('root')); //prop correcta
