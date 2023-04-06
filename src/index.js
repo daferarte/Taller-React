25adff0c55baf9d9166bf5cf42ec64ee1d94736e
daferarte <daferarte@gmail.com>
2023-04-06 11:11:09 -0500

    props por defecto

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component{
+  static defaultProps ={
+    name: "pepito" //props por defecto en caso de que no se asignen
+  }
   render(){
     console.log(this.props); //props de solo lectura no se pueden reaccionar
     return <p>Hola</p>
   }
 }
 
-render(<App name="daniel"/>, document.getElementById('root')); //definicion de props
+render(<App />, document.getElementById('root')); //definicion de props
